aa09ba1caece11d5596dac9790078027b3a2bbf4
Tomasz Szarstuk <szarsti@gmail.com>
2017-12-01 18:48:35 +0000

    WIP on gorilla compression of double floats

--- src/main/scala/com/outlyer/tslib/compression/CompressionCodec.scala
+++ src/main/scala/com/outlyer/tslib/compression/CompressionCodec.scala
@@ -1,48 +1,89 @@
 package com.outlyer.tslib.compression
 
-import scala.collection.mutable.ListBuffer
-
 import com.outlyer.tslib.TimeSeriesValue
-import scodec.bits.BitVector
-import scodec.codecs.{double, list}
-import scodec.{Attempt, Codec, DecodeResult, Err, SizeBound}
+import scodec.bits.{BitVector, BinStringSyntax}
+import scodec.codecs.{double, int8}
+import scodec.{Attempt, Codec, DecodeResult, SizeBound}
+
 
 object CompressionCodec extends Codec[List[TimeSeriesValue]] {
-
-  private lazy val floatCodec: Codec[TimeSeriesValue] = double.xmap(TimeSeriesValue.apply, _.v)
-  private lazy val dummyCodec: Codec[List[TimeSeriesValue]] = list(floatCodec)
 
   override def sizeBound = SizeBound.unknown
 
   override def encode(values: List[TimeSeriesValue]): Attempt[BitVector] = {
-    // TODO: fill buf with encoded bits
-    val buf = BitVector.empty
+    val first = values.head.v
+    var buf:BitVector = double.encode(first).require
+    var prev = buf
 
+    for (x <- values.tail) {
+      val curr = double.encode(x.v).require
 
+      if (curr == prev) {
+        buf = buf :+ false
 
+      } else {
+        buf = buf :+ true
+        val xored = prev ^ curr
+        val left_zeros = xored.indexOfSlice(bin"1")
+        val right_zeros = xored.reverse.indexOfSlice(bin"1")
 
-    // TODO: return success or failure
+        // TODO
+        // get number of trailing zeros
+        //if (prev_left_zeros >= left_zeros and prev_right_zeros >= right_zeros ) {
+        //  buf = buf :+ false ++ value_bits
+        //} else {
+
+        val offset_field = int8.encode(left_zeros.toInt).require.slice(3, 8)
+        val size = 64 - left_zeros - right_zeros
+        val size_field = int8.encode(size.toInt).require.slice(2, 8)
+
+        buf = (buf :+ true) ++ offset_field ++ size_field
+        buf = buf ++ xored.slice(left_zeros, left_zeros + size)
+      }
+      prev = curr
+    }
+
     Attempt.successful(buf)
-    Attempt.failure(Err("Failed to encode time series values"))
-
-    // delete this line after implementation
-    dummyCodec.encode(values)
   }
 
   override def decode(bits: BitVector): Attempt[DecodeResult[List[TimeSeriesValue]]] = {
-    // TODO: fill buf with encoded bits
-    val buf = new ListBuffer[TimeSeriesValue]
-    val remaining = BitVector.empty
 
+    def decode_rest(v: BitVector, acc:List[Double]): List[Double] = {
+      if (v.isEmpty) {
+        acc.reverse
 
+      } else {
+        val (hd1, v1) = v.splitAt(1)
+        val prev = acc.head
 
+        if (hd1 == bin"0") {
+          decode_rest(v1, prev :: acc)
 
+        } else {
+          val (hd2, v2) = v1.splitAt(1)
+          // TODO: handle case when (hd2 == bin"0")
 
-    // TODO: return success or failure
-    Attempt.successful(DecodeResult.apply(buf.toList, remaining))
-    Attempt.failure(Err("Failed to encode time series values"))
+          val (offset_field, v3) = v2.splitAt(5)
+          val offset = int8.decode(offset_field.padLeft(8)).require.value.toLong
+          val (size_field, v4) = v3.splitAt(6)
+          val size = int8.decode(size_field.padLeft(8)).require.value.toLong
+          val (xored_part, v5) = v4.splitAt(size)
+          val xored = (BitVector.fill(offset)(false) ++ xored_part).padRight(64)
+          val value_bin = double.encode(prev).require ^ xored
+          val value = double.decode(value_bin).require.value
+          decode_rest(v5, value :: acc)
+        }
+      }
+    }
 
-    // delete this line after implementation
-    dummyCodec.decode(bits)
+    if (bits.isEmpty) {
+      return Attempt.successful(DecodeResult(List(), BitVector.empty))
+    }
+
+    val (head, tail) = bits.splitAt(64)
+    val first_value = double.decode(head).require.value
+    val values = decode_rest(tail, List(first_value))
+    val result = DecodeResult(values.map(TimeSeriesValue), BitVector.empty)
+    Attempt.successful(result)
   }
 }
--- src/test/scala/com/outlyer/tslib/compression/CompressionCodecTest.scala
+++ src/test/scala/com/outlyer/tslib/compression/CompressionCodecTest.scala
@@ -2,16 +2,33 @@ package com.outlyer.tslib.compression
 
 import com.outlyer.tslib.TimeSeriesValue
 import org.scalacheck.{Arbitrary, Gen}
+import org.scalatest.Matchers
 
-final class CompressionCodecTest extends CodecSpec {
+
+final class CompressionCodecTest extends CodecSpec with Matchers {
     import ValuesCodecTestData._
 
-    "the CompressionCodec class" should {
-      "handle compression and decompression for timeseries values" in {
-        forAll { vs: List[TimeSeriesValue] => roundTrip(CompressionCodec, vs) }
-      }
+  "the CompressionCodec class" should {
+    "use single bit 0 for duplicated value" in {
+      val three = TimeSeriesValue(3.0)
+      val encoded = CompressionCodec.encode(List(three, three)).require
+      encoded.last should be (false)
+      encoded.length should be (65)
+    }
+
+    "use 11 prefix with 5 bits for size and new maningfull value" in {
+      val first = TimeSeriesValue(9.0)
+      val second = TimeSeriesValue(12.0)
+      val encoded = CompressionCodec.encode(List(first, second)).require
+      encoded.slice(64, 80).toBin should be ("11" + "01100" + "000011" + "101")
+      encoded.length should be (80)
+    }
+
+    "handle compression and decompression for timeseries values" in {
+      forAll { vs: List[TimeSeriesValue] => roundTrip(CompressionCodec, vs) }
     }
   }
+}
 
 object ValuesCodecTestData {
 
